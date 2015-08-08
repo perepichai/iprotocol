aecf12182ee54623c8a42bf00bba16d1aa83056e
chepon <o.perepichai@gmail.com>
2015-08-09 00:47:38 +0300

    first commit

---
+++ js/dovidnyk-storage.js
@@ -0,0 +1,15 @@
+/**
+ * Created by o.perepichay on 03.08.2015.
+ */
+var data = {};
+data.skargy = [
+    "Пісяюсь вночі ",
+    "Багато п'ю алкоголю ",
+    "Хочу полонити весь світ "
+];
+
+data.anamnezz = [
+    "test ",
+    "test2 ",
+    "test 3 "
+];
