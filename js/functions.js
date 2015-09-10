bfc8c99a7e1501d0896fabdcf175957f5889f06b
Chepon <o.perepichai@gmail.com>
2015-09-10 10:51:28 +0300

    update autofill

--- js/functions.js
+++ js/functions.js
@@ -13,19 +13,55 @@ window.addEventListener('load', function () {
         x += 1;
     }
 
+    //for (y = 0; y < storage.length; y++) {
+    //    document.querySelector('#' + storage[y]).addEventListener('click', function () {
+    //        var node = document.querySelector(".dovidnyk");
+    //        node.innerHTML = '';
+    //        for (var i = 0; i < data[this.id].length; i++) {
+    //            //console.log(data[this.id]);
+    //            var div = document.createElement("div");
+    //            var p = document.createElement("button");
+    //            p.classList.add("btn", "btn-warning", "btn-xs");
+    //            p.innerHTML = data[this.id][i];
+    //            p.addEventListener('dblclick', function() {
+    //                //var sk = document.querySelector('#skargy');
+    //                var sk = document.querySelector('#skargy');
+    //
+    //                if(sk) {
+    //                    sk.value += this.innerHTML;
+    //                } else {
+    //                    alert ('что то не так!')
+    //                }
+    //            });
+    //
+    //            div.appendChild(p);
+    //            node.appendChild(div);
+    //        }
+    //    });
+    //}
+
+    //for (var i = 0; i < data[storage[0]].length; i++) {
+    //    var div = document.createElement("div");
+    //    div.innerHTML = data[storage[0]][i];
+    //    div.addEventListener('dblclick', function() {
+    //        document.querySelector('#skargy').innerHTML += this.innerHTML;
+    //    });
+    //    var node = document.querySelector(".Dovidnyk");
+    //    node.appendChild(div);
+    //}
     for (y = 0; y < storage.length; y++) {
-        document.querySelector('#' + storage[y]).addEventListener('click', function () {
+        document.querySelector('#' + storage[y]).addEventListener('click', function (eventObj) {
+            var fieldForAutoFill = eventObj.target.id;//id обьекта события
             var node = document.querySelector(".dovidnyk");
             node.innerHTML = '';
             for (var i = 0; i < data[this.id].length; i++) {
-                //console.log(data[this.id]);
                 var div = document.createElement("div");
                 var p = document.createElement("button");
                 p.classList.add("btn", "btn-warning", "btn-xs");
                 p.innerHTML = data[this.id][i];
                 p.addEventListener('dblclick', function() {
-                    //var sk = document.querySelector('#skargy');
-                    var sk = document.querySelector('#skargy');
+
+                    var sk = document.querySelector('#' +fieldForAutoFill);
 
                     if(sk) {
                         sk.value += this.innerHTML;
@@ -39,14 +75,4 @@ window.addEventListener('load', function () {
             }
         });
     }
-
-    //for (var i = 0; i < data[storage[0]].length; i++) {
-    //    var div = document.createElement("div");
-    //    div.innerHTML = data[storage[0]][i];
-    //    div.addEventListener('dblclick', function() {
-    //        document.querySelector('#skargy').innerHTML += this.innerHTML;
-    //    });
-    //    var node = document.querySelector(".Dovidnyk");
-    //    node.appendChild(div);
-    //}
 });
